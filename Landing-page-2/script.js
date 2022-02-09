9f0ce5e7fe251f7594ce581739441c01fd266fb4
cleidianaa <cleidianapassos@outlook.pt>
2022-02-09 13:53:51 +0000

    Adicionando mais uma landing page

---
+++ Landing-page-2/script.js
@@ -0,0 +1,7 @@
+function imgSlider(anything){
+    document.querySelector('.ice-cream').src = anything
+}
+function changeCircleColor(color){
+    const circle = document.querySelector('.circle')
+    circle.style.background =color
+}
